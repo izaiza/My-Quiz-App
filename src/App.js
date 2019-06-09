bfc2217df0d33aac660dfb9b0ba55c8a4cab16ba
Iza Gawelowicz <igawelowicz@gmail.com>
2019-06-09 22:07:55 +0200

    upgrade

--- src/App.js
+++ src/App.js
@@ -4,20 +4,20 @@ import classNames from 'classnames';
 import styles from './style.scss';
 
 class App extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            questions:[
-                ["3 and 3","6"],
-                ["1 and 3","4"],
-                ["2 and 3","5"],
-                ["4 and 3","7"],
-                ["5 and 3","8"],
-                ["6 and 3","9"],
-                ["7 and 3","10"],
-                ["9 and 3","12"],
-                ["9 and 4","13"],
-                ["8 and 2","10"]
+            questions: [
+                ["3 and 3", "6"],
+                ["1 and 3", "4"],
+                ["2 and 3", "5"],
+                ["4 and 3", "7"],
+                ["5 and 3", "8"],
+                ["6 and 3", "9"],
+                ["7 and 3", "10"],
+                ["9 and 3", "12"],
+                ["9 and 4", "13"],
+                ["8 and 2", "10"]
             ],
             counter: 0,
         };
@@ -27,107 +27,96 @@ class App extends React.Component {
 
 
     Count() {
-        var newCounter = this.state.counter + 1;
-        this.setState({counter:newCounter})
+        let newCounter = this.state.counter + 1;
+        this.setState({counter: newCounter})
     }
-    render (){
+
+    render() {
         return (
             <div>
                 <h1>This is my very basic Quiz App</h1>
                 <div className="correct-answers-container">
-                <span className="correct-answers">Correct answers: {this.state.counter} / {this.state.questions.length}</span>
+                    <span
+                        className="correct-answers">Correct answers: {this.state.counter} / {this.state.questions.length}</span>
                 </div>
-                <ShowHint />
 
-                {this.state.questions.map((q,index)=> {
-                    const counter = index+1;
-                    return <Quiz Count = {this.Count}
+                {this.state.questions.map((q, index) => {
+                    const counter = index + 1;
+                    return <Quiz Count={this.Count}
                                  counter={this.state.counter}
-                                 question={'What is a sum of '+this.state.questions[index][0]+'?'}
+                                 question={'What is a sum of ' + this.state.questions[index][0] + '?'}
                                  answer={this.state.questions[index][1]}/>
                 })}
-                <div class="play-again-container">
-                <a href="http://localhost:8080/" class="play-again">PLAY AGAIN</a>
+                <div className="play-again-container">
+                    <a href="http://localhost:8080/" className="play-again">PLAY AGAIN</a>
                 </div>
             </div>
         )
     }
-}
-
-class ShowHint extends Component {
-    showHint() {
-        window.open("https://calc.pl/matematyka/matematyczny", "_blank");
-    }
-
-    render() {
-        return <div class="need-help-container">
-            <button onClick={this.showHint} className="help">Help yourself &#127808;</button>
-            </div>
-    }
-}
-
-class Animation extends Component {
-    render(){
-        return(
-            this.props.message
-        )
-    }
-}
-
-class IsMessageRight extends Component {
-    render(){
-        return (
-            <div>{this.props.response}</div>
-        )
-    }
-
 }
 
 class Quiz extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             userAnswer: '',
             isDisabled: '',
             className: 'init',
             buttonValue: 'Am I right?',
-            hint: "https://calc.pl/matematyka/matematyczny",
-            showSth: '',
-            backgroundColor: ''
+            backgroundColor: '',
+            message: ''
         };
+
         this.checkAnswer = this.checkAnswer.bind(this);
         this.typeAnswer = this.typeAnswer.bind(this);
     }
+
     typeAnswer(e) {
-        this.setState({userAnswer:e.target.value});
+        this.setState({userAnswer: e.target.value});
     }
-    checkAnswer (e) {
+
+    checkAnswer(e) {
         e.preventDefault();
-        var isCorrect = this.state.userAnswer === this.props.answer;
+        let isCorrect = this.state.userAnswer === this.props.answer;
         if (isCorrect) {
             this.props.Count();
-            this.setState({isDisabled:"disabled"});
-            this.setState({buttonValue:"Yes!"});
-            this.setState({showSth:"WOW"});
-            this.setState({backgroundColor: 'green'})
+            this.setState({
+                isDisabled: "disabled",
+                buttonValue: "Yes!",
+                backgroundColor: "#41f47a",
+                message: 'Well done!'
+            });
         } else {
-            this.setState({userAnswer:"Wrong answer"});
-            this.setState({buttonValue:"No"});
-            this.setState({isDisabled:"disabled"});
+            this.setState({
+                userAnswer: "Wrong answer",
+                buttonValue: "No",
+                isDisabled: "disabled",
+                backgroundColor: "#f46542",
+                message: 'Not this time'
+            });
         }
     }
-    render (){
+
+    render() {
         return (
             <form onSubmit={this.checkAnswer} className="class-added">
-                <div className="form-group">
-                    <label htmlFor={"questions"} >{this.props.question} <br/>
-                        <input name="questions"
-                               className={classNames(this.state.className)}
-                               value={this.state.userAnswer}
-                               onChange={this.typeAnswer}
-                               disabled = {(this.state.isDisabled)? "disabled" : ""}/>
+                <div className="form">
+                    <label htmlFor={"questions"}>{this.props.question} <br/>
+                        <input
+                            name="questions"
+                            className={classNames(this.state.className)}
+                            value={this.state.userAnswer}
+                            onChange={this.typeAnswer}
+                            style={{backgroundColor: this.state.backgroundColor}}
+                            disabled={(this.state.isDisabled) ? "disabled" : ""}
+                            autoComplete="off"
+                        />
                     </label>
-                <input className="submit-input" type="submit" value={this.state.buttonValue} disabled = {(this.state.isDisabled)? "disabled" : ""} />
+                    <input
+                        className="submit-input" type="submit"
+                        value={this.state.buttonValue} disabled={(this.state.isDisabled) ? "disabled" : ""}
+                    />
+                    <span className="feedback-msg">{this.state.message}</span>
                 </div>
             </form>
         );
@@ -135,6 +124,6 @@ class Quiz extends React.Component {
 }
 
 ReactDOM.render(
-    <App />,
+    <App/>,
     document.getElementById('app')
 );
