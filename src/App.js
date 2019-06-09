866b718deec1439b6703487adb51edb21d79545c
Iza Gawelowicz <igawelowicz@gmail.com>
2019-06-09 11:02:11 +0200

    Quiz

--- src/App.js
+++ src/App.js
@@ -9,15 +9,15 @@ class App extends React.Component {
         this.state = {
             questions:[
                 ["3 and 3","6"],
-                ["1 and 3","6"],
-                ["2 and 3","6"],
-                ["4 and 3","6"],
-                ["5 and 3","6"],
-                ["6 and 3","6"],
-                ["7 and 3","6"],
-                ["9 and 3","6"],
-                ["9 and 3","6"],
-                ["20 and 3","23"]
+                ["1 and 3","4"],
+                ["2 and 3","5"],
+                ["4 and 3","7"],
+                ["5 and 3","8"],
+                ["6 and 3","9"],
+                ["7 and 3","10"],
+                ["9 and 3","12"],
+                ["9 and 4","13"],
+                ["8 and 2","10"]
             ],
             counter: 0,
         };
@@ -34,18 +34,21 @@ class App extends React.Component {
         return (
             <div>
                 <h1>This is my very basic Quiz App</h1>
-
-                <span className="correct-answers">Correct answers: {this.state.counter} / {this.state.questions.length}</span> <br/>
+                <div className="correct-answers-container">
+                <span className="correct-answers">Correct answers: {this.state.counter} / {this.state.questions.length}</span>
+                </div>
                 <ShowHint />
 
-                {this.state.questions.map((qa,index)=> {
+                {this.state.questions.map((q,index)=> {
                     const counter = index+1;
                     return <Quiz Count = {this.Count}
                                  counter={this.state.counter}
-                                 question={counter+'. What is a sum of '+this.state.questions[index][0]+'?'}
+                                 question={'What is a sum of '+this.state.questions[index][0]+'?'}
                                  answer={this.state.questions[index][1]}/>
                 })}
-                <a href="http://localhost:8080/" class="play-again">Play again</a>
+                <div class="play-again-container">
+                <a href="http://localhost:8080/" class="play-again">PLAY AGAIN</a>
+                </div>
             </div>
         )
     }
@@ -57,7 +60,9 @@ class ShowHint extends Component {
     }
 
     render() {
-        return <button onClick={this.showHint} className="help">Help yourself &#127808;</button>;
+        return <div class="need-help-container">
+            <button onClick={this.showHint} className="help">Help yourself &#127808;</button>
+            </div>
     }
 }
 
@@ -87,7 +92,8 @@ class Quiz extends React.Component {
             className: 'init',
             buttonValue: 'Am I right?',
             hint: "https://calc.pl/matematyka/matematyczny",
-            showSth: ''
+            showSth: '',
+            backgroundColor: ''
         };
         this.checkAnswer = this.checkAnswer.bind(this);
         this.typeAnswer = this.typeAnswer.bind(this);
@@ -102,10 +108,12 @@ class Quiz extends React.Component {
             this.props.Count();
             this.setState({isDisabled:"disabled"});
             this.setState({buttonValue:"Yes!"});
-            this.setState({showSth:"WOWOWOW"})
+            this.setState({showSth:"WOW"});
+            this.setState({backgroundColor: 'green'})
         } else {
-            this.setState({userAnswer:"Try again"});
-            this.setState({buttonValue:"No!"});
+            this.setState({userAnswer:"Wrong answer"});
+            this.setState({buttonValue:"No"});
+            this.setState({isDisabled:"disabled"});
         }
     }
     render (){
@@ -119,9 +127,8 @@ class Quiz extends React.Component {
                                onChange={this.typeAnswer}
                                disabled = {(this.state.isDisabled)? "disabled" : ""}/>
                     </label>
-                    <Animation message={'??'}/>
+                <input className="submit-input" type="submit" value={this.state.buttonValue} disabled = {(this.state.isDisabled)? "disabled" : ""} />
                 </div>
-                <input type="submit" value={this.state.buttonValue} disabled = {(this.state.isDisabled)? "disabled" : ""} />
             </form>
         );
     }
